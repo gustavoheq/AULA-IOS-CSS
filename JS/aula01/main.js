f6590b8c3bd8e69579029b8941af2cf4afad08ed
Gustavo <gustavomirandaoliveira@outlook.com>
2024-04-30 14:44:59 -0300

    Olá, Mundo js

--- JS/aula01/main.js
+++ JS/aula01/main.js
@@ -1,7 +1,22 @@
 // let
 // var 
 // const
+//String
+//Number
+// Boolean
 
-let nome = "Luis" //Texto - String
+let nome = "Luis" //String
 let idade = 7.8 //Number
-let praia = false // Boolean 
+let praia = false // Boolean
+
+let mNome = "Gustavo"
+let mIdade = 27
+let Casa = false
+
+let pet = "Hugo"
+let tipoPet = 'Cachorro'
+let idadePet = 5
+let RD = `Shih-Tzu`
+
+console.log("Olá, Mundo! ");
+
